11690b7a3dbcf9a96efe9c210bbd1c72552966e7
simeunwoo <simeunwoo11@naver.com>
2024-11-06 15:28:02 +0900

    1106

--- SpringReactQueryLastProject/src/main/java/com/sist/web/restcontroller/NewsRestController.java
+++ SpringReactQueryLastProject/src/main/java/com/sist/web/restcontroller/NewsRestController.java
@@ -3,23 +3,23 @@ package com.sist.web.restcontroller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import com.sist.web.entity.*;
 import com.sist.web.manager.*;
 import java.util.*;
+
 @RestController
+@CrossOrigin(origins = "*")
 public class NewsRestController {
    @Autowired
    private NewsSearchManager mgr;
    
-   @GetMapping("/news/list/{fd}")
-   public ResponseEntity<Map> news_list(@PathVariable("fd") String fd)
+   @GetMapping("/news/find/{fd}")
+   public ResponseEntity<Map> news_find(@PathVariable("fd") String fd)
    {
-	   if(fd==null)
-		   fd="맛집";
-	   
 	   Map map=new HashMap();
 	   try
 	   {
