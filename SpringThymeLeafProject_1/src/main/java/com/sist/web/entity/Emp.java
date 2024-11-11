6a084180bd27cddfaef5991f9acecfbdb5ba0f7c
simeunwoo <simeunwoo11@naver.com>
2024-11-11 10:33:19 +0900

    1111

--- SpringThymeLeafProject_1/src/main/java/com/sist/web/entity/Emp.java
+++ SpringThymeLeafProject_1/src/main/java/com/sist/web/entity/Emp.java
@@ -1,7 +1,11 @@
 package com.sist.web.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 // 클래스명과 테이블명이 동일하면 => table명을 지정하지 않아도 된다
@@ -11,7 +15,13 @@ public class Emp {
 
 	@Id
 	private int empno;
-	private int sal,deptno;
 	private String ename,job,hiredate,mgr,comm;
 	// csv => 1) 값 중에 null이 존재 2) 날짜 => 문자열 (text)
+	private int sal;
+	@Column(insertable=false,updatable=false)
+	private int deptno;
+	@ManyToOne(fetch=FetchType.EAGER)
+	// LAZE (지연), EAGER (즉시 로딩)
+	@JoinColumn(name="deptno",referencedColumnName="deptno")
+	private Dept dept;
 }
